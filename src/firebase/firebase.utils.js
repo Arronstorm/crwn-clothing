dcbb809cc98e2d5cdc1a09c27c3771167c2625cf
Abhishek Raj Nair <abhishekrajnair1@gmail.com>
2021-09-02 22:46:07 +0530

    created sign up using email and password

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -15,8 +15,27 @@ const config = {
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
-  console.log(firebase.doc("users/128cshds"));
-  ````;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console("error in Loggin In", error.message);
+    }
+  }
+
+  return userRef;
 };
 
 firebase.initializeApp(config);
