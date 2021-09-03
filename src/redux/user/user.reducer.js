99a508603003d214b325c6084f2fabb491ef4fcd
Abhishek Raj Nair <abhishekrajnair1@gmail.com>
2021-09-03 20:44:29 +0530

    added cart icon and cart dropdown feature while pressing the cart icon

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,4 +1,4 @@
-import { userActionTypes } from "./user.types";
+import { UserActionTypes } from "./user.types";
 
 const INITIAL_STATE = {
   currentUser: null,
@@ -6,10 +6,10 @@ const INITIAL_STATE = {
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case userActionTypes.SET_CURRENT_USER:
+    case UserActionTypes.SET_CURRENT_USER:
       return {
         ...state,
-        currentUser: action.paylod,
+        currentUser: action.payload,
       };
     default:
       return state;
